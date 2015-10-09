dc6ae0f877086e2a0064ac5bf5431caed9878be2
dmccue <dmccue@gmail.com>
2015-10-09 22:11:40 +0100

    minor tweaks

--- get_routes.py
+++ get_routes.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python
 
+
 import requests, sys, json, csv
 
 
-def get_routesatstopid(input_id):
+header = [
+  'route_id',
+  'agency_id',
+  'route_short_name',
+  'route_long_name',
+  'route_desc',
+  'route_type',
+  'route_url',
+  'route_color',
+  'route_text_color'
+]
 
-    payload = {
-        'locationServerActive': '1',
-        'type_dm': 'stop',
-        'limit': '999999',
-        'outputFormat': 'JSON',
-        'coordOutputFormat': 'WGS84',
-        'language': 'en',
-        'name_dm': input_id
-    #     ,
-    #     'outputFormat': 'JSON',
+
+def get_routesatstopid(input_id):
+  routes = []
+
+  payload = {
+    'locationServerActive': '1',
+    'type_dm': 'stop',
+    'limit': '999999',
+    'outputFormat': 'JSON',
+    'coordOutputFormat': 'WGS84',
+    'language': 'en',
+    'name_dm': input_id
     #     'mode': 'direct',
     #     'mergeDep': '1',
     #     'useAllStops': '1',
@@ -23,8 +36,6 @@ def get_routesatstopid(input_id):
     #     'useRealtime': '1',
     #     'depType': 'stopEvents',
     #     'includeCompleteStopSeq': '0',
-    #     'name_dm': '10005183',
-    #     'type_dm': 'stop',
     #     'imparedOptionsActive': '1',
     #     'googleAnalytics': 'false',
     #     'excludedMeans': 'checkbox',
@@ -38,76 +49,52 @@ def get_routesatstopid(input_id):
     #     'ptOptionsActive': '1',
     #     'limit': '30',
     #     'snapHouseNum': '1'
-    }
-    response = requests.get('http://journeyplanner.translink.co.uk/android/XML_DM_REQUEST', params=payload).text.encode('utf-8')
-    root = json.loads(response)
+  }
+  response = requests.get('http://journeyplanner.translink.co.uk/android/XML_DM_REQUEST', params=payload).text.encode('utf-8')
+  root = json.loads(response)
 
-    routes = []
-    # print json.dumps(root['servingLines']['lines'], sort_keys = False, indent = 2)
+  # print json.dumps(root['servingLines']['lines'], sort_keys = False, indent = 2)
+
+  if not root['servingLines']['lines']:
+    return []
+
+  if len(root['servingLines']['lines']) == 1:
+    root['servingLines']['lines'] = [root['servingLines']['lines']['line']]
+
+  for line in root['servingLines']['lines']:
+    row = [
+      line['mode']['diva']['stateless'],
+      '',
+      line['mode']['number'],
+      line['mode']['name'],
+      line['mode']['desc'],
+      line['mode']['code'],
+      '',
+      '',
+      ''
+    ]
+    routes.append(row)
+  return routes
 
 
-    if not root['servingLines']['lines']:
-        return []
+def write_headertofile(in_csvwrite, in_header):
+  in_csvwrite.writerow(in_header)
 
-    if len(root['servingLines']['lines']) == 1:
-        root['servingLines']['lines'] = [root['servingLines']['lines']['line']]
 
-    for line in root['servingLines']['lines']:
-        row = [
-            line['mode']['diva']['stateless'],
-            '',
-            line['mode']['number'],
-            line['mode']['name'],
-            line['mode']['desc'],
-            line['mode']['code'],
-            '',
-            '',
-            ''
-        ]
-        routes.append(row)
-    return routes
-
-def write_headertofile():
-    header = ['route_id','agency_id','route_short_name','route_long_name','route_desc','route_type','route_url','route_color','route_text_color']
-    with open('GTFS/routes.txt', 'wb') as f:
-        csvwrite = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csvwrite.writerow(header)
-
-def write_routestofile(input_routes):
-    with open('GTFS/routes.txt', 'wb') as f:
-        csvwrite = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        for row in input_routes:
-            csvwrite.writerow(row)
+def write_rowstofile(in_csvwrite, in_rows):
+  for row in in_rows:
+    in_csvwrite.writerow(row)
 
 
 # Main
-write_headertofile()
-with open('GTFS/stops.txt', 'rb') as csvfile:
-    csvread = csv.reader(csvfile, delimiter=',', quotechar='"')
-    for row in csvread:
-        stopid = str(row[0])
-        print "Getting routes at stop: " + stopid
-        returned_routes = get_routesatstopid(stopid)
-        print "Found " + str(len(returned_routes)) + " routes"
-        write_routestofile(returned_routes)
+with open('GTFS/routes.txt', 'wb') as f:
+  csvwrite = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+  write_headertofile(csvwrite)
 
-
-#(' + line['mode']['destID'] + ', ' + line['mode']['destination'] + ')'
-#    print json.dumps(line)
-
-
-# for key,val in root.items():
-#     if key and val:
-#       print str(key) + ' = ' + str(len(str(val)))
-#     elif key:
-#       print str(key)
-#
-# #print json.dumps(root, sort_keys = False, indent = 4)
-# sys.exit(1)
-#
-#
-# print 'arr: not needed'
-# print 'parameters: not needed'
-# print 'dateTime: ' + str(root['dateTime'].keys())
-# for item in root['dateRange']:
-#     print str(item)
+  with open('GTFS/stops.txt', 'rb') as csvfile:
+    for row in csv.reader(csvfile, delimiter=',', quotechar='"'):
+      stopid = str(row[0])
+      print "Getting routes at stop: " + stopid
+      returned_routes = get_routesatstopid(stopid)
+      print "Found " + str(len(returned_routes)) + " routes"
+      write_rowstofile(csvwrite, returned_routes)
